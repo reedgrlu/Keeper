26da960530bd7c89ffd1b96d18f53c6d4f86c612
reedgrlu <51003043+reedgrlu@users.noreply.github.com>
2020-05-10 23:38:54 -0700

    implemented behavior of module upon button being clicked, as well as how to handle the input to the text sections

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import Note from "./Note";
 
-function CreateArea() {
-  return (
-    <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+function CreateArea(props) {
+    const [noteTitle, setNoteTitle] = useState("");
+    const [noteContent, setNoteContent] = useState("");
+    
+    function changeTitle(event){
+        setNoteTitle(event.target.value);
+    }
+
+    function changeContent(event){
+        setNoteContent(event.target.value);
+    }
+  
+    return (
+        <div>
+        <form>
+            <input name="title" placeholder="Title" onChange={changeTitle} value={noteTitle} />
+            <textarea name="content" placeholder="Take a note..." rows="3" onChange ={changeContent} value={noteContent} />
+            <button onClick = {event => {
+                props.addNote([noteTitle,noteContent]);
+                setNoteTitle("");
+                setNoteContent("");
+
+                event.preventDefault();
+                
+            }}>Add</button>
+        </form>
+        </div>
+    );
 }
 
 export default CreateArea;
