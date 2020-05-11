d5a7c499193e5d4fd1164a56e3bb4b40d7e04319
reedgrlu <51003043+reedgrlu@users.noreply.github.com>
2020-05-10 22:53:53 -0700

    Incorperated the CreateArea module in App and updated the CSS file for its styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
-import notes from "../notes";
+// import notes from "../notes";
+import CreateArea from "./CreateArea";
 
-console.log(notes)
+// console.log(notes)
 
 function App(){
     return (
         <div>
             <Header />
-            {notes.map(note => <Note key = {note.key} title = {note.title} content = {note.content} /> )}
+            <CreateArea />
+            <Note key={1} title="Note title" content="Note content" />
             <Footer />
         </div>
     );
