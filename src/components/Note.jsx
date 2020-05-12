117631bacadac733a9d051007f82df7f9bf2a348
reedgrlu <51003043+reedgrlu@users.noreply.github.com>
2020-05-11 22:10:43 -0700

    Added discard button and added its onClick behavior to pass the props.id as an input to props.onDiscard()

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-function Note(props){
+function Note(props) {
     return (
-    <div className="note">
-        <h1>{props.title}</h1>
-        <p>{props.content}</p>
-    </div>)
+        <div className="note">
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+            <button
+                onClick={() => {
+                    props.onDiscard(props.id);
+                }}
+            >
+                DISCARD
+      </button>
+        </div>)
 }
 
 export default Note;
